674c004b663f7eec6ac03a264d4555ccc4b34f19
MichalSzopa <szopqa96@gmail.com>
2017-08-22 21:16:48 +0200

    Authentication Finished - logging via google account is now available

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys.js');
 require('./models/user.js');
 require('./services/passport.js');
-
-
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+// cookies middleware
+app.use(
+	cookieSession({
+		maxAge : 30 * 24 * 60 * 60 * 1000,
+		keys : [keys.cookieKey]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 require('./routes/authRoutes.js')(app);
+
 
 
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -1,22 +1,36 @@
 const passport = require('passport');
 const  GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys.js');
 const monogoose = require('mongoose');
+const keys = require('../config/keys.js');
 
 const User = monogoose.model('users');
+
+passport.serializeUser((user,done) => {
+	done(null,user.id);
+});
+
+passport.deserializeUser((id,done) => {
+	User.findById(id)
+		.then(user => {
+			done(null,user);
+		});
+});
 
 passport.use(new GoogleStrategy({
 		clientID: keys.googleClientID,
 		clientSecret : keys.googleClientSecret,
 		callbackURL : '/auth/google/callback'
-	}, (accessToken, refreshToken, profile, done) => {
+	},
+	(accessToken, refreshToken, profile, done) => {
 		User.findOne({googleId:profile.id})
 			.then((existingUser) => {
 				if(existingUser){
 					//User has already registered
 					done(null, existingUser);
 				} else {
-					new User({googleId:profile.id}).save()
+					//Adding new user to database
+					new User({googleId:profile.id})
+						.save()
 						.then(user => done(null,user));
 				}
 			});
