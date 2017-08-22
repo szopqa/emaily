7abcc48cb43ec2b144845c5ea2947329605b66b7
MichalSzopa <szopqa96@gmail.com>
2017-08-22 23:28:05 +0200

    Code refactor - Changed promisses into async await syntax

--- services/passport.js
+++ services/passport.js
@@ -21,19 +21,15 @@ passport.use(new GoogleStrategy({
 		clientSecret : keys.googleClientSecret,
 		callbackURL : '/auth/google/callback'
 	},
-	(accessToken, refreshToken, profile, done) => {
-		User.findOne({googleId:profile.id})
-			.then((existingUser) => {
-				if(existingUser){
-					//User has already registered
-					done(null, existingUser);
-				} else {
-					//Adding new user to database
-					new User({googleId:profile.id})
-						.save()
-						.then(user => done(null,user));
-				}
-			});
+	async (accessToken, refreshToken, profile, done) => {
+		const existingUser = await User.findOne({googleId:profile.id});
+
+			if(existingUser){
+			return done(null, existingUser);
+			}
+
+			const user = await new User({googleId:profile.id}).save();
+			done(null,user);
 	})
 );
 
